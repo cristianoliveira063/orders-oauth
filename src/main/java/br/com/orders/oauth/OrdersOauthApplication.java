34c79fa000d0e7d45ce1f8370a3cb36fd67852a5
cristianoliveira063 <cristianoliveira063@hotmail.com>
2021-03-20 23:41:20 -0400

    First commit

---
+++ src/main/java/br/com/orders/oauth/OrdersOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.orders.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersOauthApplication.class, args);
+    }
+
+}
