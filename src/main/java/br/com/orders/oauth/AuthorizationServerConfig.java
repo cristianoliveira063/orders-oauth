c5a367f6a64570c93dd92ba242313f75d057c557
Cristiano063 <cristianoliveira063@gmail.com>
2021-03-26 23:48:51 -0400

    Configurando o Refresh Token Grant Type no Authorization Server

--- src/main/java/br/com/orders/oauth/AuthorizationServerConfig.java
+++ src/main/java/br/com/orders/oauth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.orders.oauth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                 .withClient("orders-web")
                 .secret(passwordEncoder.encode("web123"))
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password","refresh_token")
                 .scopes("write", "read")
-                .accessTokenValiditySeconds(60 * 60 * 6); // 6 horas (padrão é 12 horas)
+                .accessTokenValiditySeconds(60 * 60 * 6); // 6 horas (padrão é 12 horas)p
     }
 
     @Override
@@ -39,7 +44,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
+
+
 
 }
